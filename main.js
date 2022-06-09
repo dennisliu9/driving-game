2ba57261540b7e2b3bc77a192495c788c923497a
Dennis Liu <dennis.liu9@gmail.com>
2022-06-09 23:33:54 +0000

    Revamped code to have direction controlled by just left and right arrows

--- main.js
+++ main.js
@@ -1,29 +1,19 @@
 var carData = {
   $carObj: document.querySelector('#p1-car'),
-  orientation: 'east',
-  rotateCar: function () {
-    this.$carObj.classList.remove('face-north');
-    this.$carObj.classList.remove('face-south');
-    this.$carObj.classList.remove('face-east');
-    this.$carObj.classList.remove('face-west');
-    this.$carObj.classList.add('face-' + this.orientation);
+  orientation: 0,
+  rotateCar: function (turnKey) {
+    if (turnKey === 'ArrowRight') {
+      this.orientation += 90;
+    } else if (turnKey === 'ArrowLeft') {
+      this.orientation += -90;
+    } else if (turnKey === 'ArrowDown') {
+      this.orientation += (Math.random() < 0.5 ? 1 : -1) * 360 * Math.floor(Math.random() * 4) + 180;
+    }
+    this.$carObj.style.transform = 'rotate(' + this.orientation + 'deg)';
   }
 };
 
-// var $car = document.querySelector('#p1-car');
-
 document.addEventListener('keydown', function (event) {
   var keyPressed = event.key;
-
-  if (keyPressed === 'ArrowRight') {
-    carData.orientation = 'east';
-  } else if (keyPressed === 'ArrowDown') {
-    carData.orientation = 'south';
-  } else if (keyPressed === 'ArrowLeft') {
-    carData.orientation = 'west';
-  } else if (keyPressed === 'ArrowUp') {
-    carData.orientation = 'north';
-  }
-
-  carData.rotateCar();
+  carData.rotateCar(keyPressed);
 });
