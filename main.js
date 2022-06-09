82954a794f0fc364f7bd20e3a8c8f5cc415da589
Dennis Liu <dennis.liu9@gmail.com>
2022-06-09 21:49:47 +0000

    Added event listener for keypress with code to update property in data object, added car DOM object to a property in the data object, added method to data object for applying the orientation to the DOM object

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 var carData = {
-  orientation: 'east'
+  $carObj: document.querySelector('#p1-car'),
+  orientation: 'east',
+  rotateCar: function () {
+    this.$carObj.classList.remove('face-north');
+    this.$carObj.classList.remove('face-south');
+    this.$carObj.classList.remove('face-east');
+    this.$carObj.classList.remove('face-west');
+    this.$carObj.classList.add('face-' + this.orientation);
+  }
 };
+
+// var $car = document.querySelector('#p1-car');
+
+document.addEventListener('keydown', function (event) {
+  var keyPressed = event.key;
+
+  if (keyPressed === 'ArrowRight') {
+    carData.orientation = 'east';
+  } else if (keyPressed === 'ArrowDown') {
+    carData.orientation = 'south';
+  } else if (keyPressed === 'ArrowLeft') {
+    carData.orientation = 'west';
+  } else if (keyPressed === 'ArrowUp') {
+    carData.orientation = 'north';
+  }
+
+  carData.rotateCar();
+});
